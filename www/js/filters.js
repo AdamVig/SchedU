2853b7f9ef55993859ae354a92f4fff8260ab732
Adam Vigneaux <adamvigneaux@gmail.com>
2014-11-07 10:59:49 -0500

    Initial commit
    Filters: sentence case (capitalize string in sentence case), class days
    (convert array of strings into array of integers for alternate class
    days), range (generate integer array of given length, start to end),
    phone number (add dashes while typing and prevent being longer than 12
    digits)
    Directive: period (WIP)

---
+++ www/js/filters.js
@@ -0,0 +1,97 @@
+angular.module('schedu.filters', [])
+ 
+.directive('period', function ($compile) {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/period.html',
+    scope: {
+      letter: "="
+    },
+    link: function (scope, elem, attrs) {
+
+      // Replace %% character with current period letter
+      elem.html(elem[0].innerHTML.replace("ltr", scope.letter));
+
+      // Compile template
+      $compile(elem.contents())(scope);
+    }
+  }
+})
+
+.filter('sentencecase', function () {
+  
+  return function (sentence) {
+    var sentenceWords = sentence.split(" ");
+    var capitalizedSentence = "";
+    
+    _.each(sentenceWords, function (word, wordNum) {
+
+      capitalizedSentence += word.substring(0,1).toUpperCase();
+      capitalizedSentence += word.slice(1);
+
+      // Add space if not last word in sentence
+      if (wordNum + 1 < sentenceWords.length) {
+        capitalizedSentence += " ";
+      }
+    });
+
+    return capitalizedSentence;
+  };
+})
+
+.filter('classdays', function () {
+
+  return function (origDays) {
+    var days = [];
+
+    // Remove null values
+    origDays = _.compact(origDays);
+
+    // Create list of integer days
+    _.each(origDays, function (day) {
+      days.push(parseInt(day));
+    });
+
+    return days;
+
+  };
+})
+
+.filter('range', function() {
+  return function(input, start, end) {
+    start = parseInt(start);
+    end = parseInt(end);
+    for (var i=start; i<=end; i++)
+      input.push(i);
+    return input;
+  };
+})
+
+.filter('phonenumber', function () {
+      
+  return function (number) {
+    /* 
+    @param {Number | String} number - Number that will be formatted as telephone number
+    Returns formatted number: (###) ###-####
+      if number.length < 4: ###
+      else if number.length < 7: (###) ###
+    */
+    if (!number) return '';
+
+    var formattedNumber = number;
+
+    // # (###) ###-#### as c (area) front-end
+    var area = number.substring(0,3);
+    var front = number.substring(3, 6);
+    var end = number.substring(6, 10);
+
+    if (front) {
+      formattedNumber = area + "-" + front;  
+    }
+    if (end) {
+      formattedNumber += "-" + end;
+    }
+
+    return formattedNumber;
+  };
+});

a097966ebcd4f4a02802487749e6e24f91313deb
Adam Vigneaux <adamvigneaux@gmail.com>
2015-01-18 00:22:15 -0500

    Refactor dbUrl into an object named DatabaseConstant

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,3 +1,7 @@
 constants.constant("appVersion", "1.4.0")
 .constant("dismissal", moment().hour(14).minute(20).second(0))
-.constant("dbUrl", "https://orgalaskelldintystarlinu:akEwnCIxXqksQdC8CqsyiSR2@schedu.cloudant.com/");
+.constant("DatabaseConstant", {
+  "url": "https://schedu.cloudant.com/",
+  "username": "orgalaskelldintystarlinu",
+  "password": "akEwnCIxXqksQdC8CqsyiSR2"
+});

d3b25aee02624945ab8182bfd2116c8bfad94bcd
Adam Vigneaux <adamvigneaux@gmail.com>
2015-01-18 00:44:36 -0500

    Add bug fix for random, multiple lunch icon appearances

--- www/js/controllers/ScheduleController.js
+++ www/js/controllers/ScheduleController.js
@@ -15,6 +15,27 @@ controllers.controller('ScheduleController', function($state, $q, $ionicPopup, L
   // Get user data object or "undefined"
   var localUser = StorageService.getUser();
 
+  // Remove 'lunch' and 'letter' keys
+  // @BUG behavior, not sure what causes it
+  _.each(localUser, function (value, key) {
+    if (_.contains(['a', 'b', 'c', 'd', 'e', 'f', 'g'], key)) {
+      if (localUser[key].letter) {
+        delete localUser[key].letter;
+      }
+      if (localUser[key].lunch) {
+        delete localUser[key].lunch;
+      }
+      if (localUser[key].alternate) {
+        if (localUser[key].alternate.lunch) {
+          delete localUser[key].alternate.lunch;
+        }
+        if (localUser[key].alternate.letter) {
+          delete localUser[key].alternate.lunch;
+        }
+      }
+    }
+  });
+
   // No local user, redirect to login page
   if (!localUser) {
 
@@ -27,8 +48,8 @@ controllers.controller('ScheduleController', function($state, $q, $ionicPopup, L
 
     DatabaseFactory.user.get(localUser._id).then(function (response) {
 
+      // Update local data if different on server
       if (response.data._rev != localUser._rev) {
-
         localUser = response.data;
         StorageService.storeUser(localUser);
       }
@@ -47,10 +68,12 @@ controllers.controller('ScheduleController', function($state, $q, $ionicPopup, L
         schedule.noUserFound = true;
       }
     }).then(function (response) {
-
-      localUser._rev = response.data.rev;
-      StorageService.storeUser(localUser);
-
+      if (response) {
+        localUser._rev = response.data.rev;
+        StorageService.storeUser(localUser);
+      } else {
+        console.error("Couldn't update user.");
+      }
     });
 
     var date = DateFactory.currentDay();

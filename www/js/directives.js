8880e00c2bce65a4ba22a904e34b6ea9ce0a9178
Adam Vigneaux <adamvigneaux@gmail.com>
2015-01-20 00:09:58 -0500

    Fix period directive
    It was simpler than I expected!

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,12 +1,6 @@
 directives.directive('period', function ($compile) {
   return {
     restrict: 'E',
-    templateUrl: 'html/_period.html',
-    scope: {
-      letter: "="
-    },
-    link: function (scope, elem, attrs) {
- 
-    }
+    templateUrl: 'html/_period.html'
   }
 });

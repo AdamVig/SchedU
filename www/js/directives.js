7c603393511560c82c36032233c2510f3ce3fc13
Adam Vigneaux <adamvigneaux@gmail.com>
2015-01-22 20:40:23 -0500

    Add edit-period directive
    - edit-period is a derivative of "period" but used just for /edit

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,6 +1,19 @@
-directives.directive('period', function ($compile) {
+directives.directive('period', [function () {
   return {
     restrict: 'E',
     templateUrl: 'html/_period.html'
   }
-});
+}])
+
+.directive('edit-period', [function () {
+  return {
+    restrict: 'E',
+    require: '^form',
+    scope: {
+      periodLetter: '@',
+      currentClass: '=',
+      currentClassForm: '='
+    },
+    templateUrl: 'html/_editperiod.html'
+  }
+}]);
